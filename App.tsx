7d71b5ba89a3b8cf0136c77e3e6b7f0fc0338977
FHIRFLY <richard.braman@fhirfly.io>
2025-05-03 09:12:51 -0400

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { HookForm } from './components/HookForm';
+
+function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>CDS Hooks Client with SMART Launch</h1>
+      <HookForm />
+    </div>
+  );
+}
+
+export default App;
